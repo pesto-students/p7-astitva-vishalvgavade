9dced9cf91367f2e151a8d3152d2119698b824bc
Vishal <vishalgawade231299@gmail.com>
2022-11-28 17:48:00 +0530

    modified some changes

--- Week-3/assignment.js
+++ Week-3/assignment.js
@@ -1,13 +1,14 @@
-function add(...rest) {
+function add(...rest) {              //Add function
   var sum=0
   for(var i=0;i<rest.length;i++){
     sum=sum+rest[i];
   }
   return sum;
 }
-function memoize(fun) {
-  let cache = {};
-  return function (...args) {
+let cache = {};                     //declared empty array
+function memoize(fun) {            //Memoize function
+  return function (...args){
+    console.log(cache);
     if (args.length==1) {
       return args[0];
     } else if ((args) in cache) {
@@ -17,11 +18,14 @@ function memoize(fun) {
       cache[args] = sum;
       return sum;
     }
-    console.log(cache);
-  };
+ };
 }
-
-const memoizeAdd = memoize(add);
+console.log(cache);
+const memoizeAdd = memoize(add);            
+console.time();
+console.log(memoizeAdd(100,100,100));
+console.log(memoizeAdd(100,100,100,100));
+console.timeEnd();
 console.time();
 console.log(memoizeAdd(100,100,100));
 console.timeEnd();
