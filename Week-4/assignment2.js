c7e4114162ee49149b1c6364261cd2cfdac27a82
Vishal <vishalgawade231299@gmail.com>
2022-12-11 19:59:02 +0530

    added all files

--- Week-4/assignment2.js
+++ Week-4/assignment2.js
@@ -1,8 +1,18 @@
-Var Person = function() {};
-Person.prototype.initialize=function(name,age){
-    this.name=name;this.age=age;
+let Person = function (){
 }
-// TODO: create the class Teacher and a method teach
-var him=newTeacher();
-him.initialize("Adam",45);
-him.teach("Inheritance");
+Person.prototype.initialize = function(name,age){
+    // console.log(name,age)
+    // console.log("initialize")
+    // console.log(this)
+    this.name = name;
+    this.age = age;
+};
+class Teacher extends Person {
+    teach(subject){
+      return `${this.name} is now teaching ${subject}`
+    }
+}
+
+let him = new Teacher();
+him.initialize("omkar",45);
+console.log(him.teach("inheritance"))
